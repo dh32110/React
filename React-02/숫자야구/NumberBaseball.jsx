e4ad7aacd55b98f3284c94002ac5882f5c3e99ee
dh32110 <dh32110@naver.com>
2023-06-13 22:29:21 +0900

    React: numberbaseball-func

--- React-02/숫자야구/NumberBaseball.jsx
+++ React-02/숫자야구/NumberBaseball.jsx
@@ -1,7 +1,7 @@
-import React,{ Component } from 'react';
+import React,{ useState } from 'react';
 import Try from '/Try';
 
-function getNumbers(){ // 숫자 네개를 겹치지 않고 랜덤하게 뽑는 함수, this를 사용하지 않는 경우 함수를 class 밖에서 선언할 수 있다.
+function getNumbers(){ // 숫자 네개를 겹치지 않고 랜덤하게 뽑는 함수, this를 사용하지 않는 경우 함수를 class 밖에서 선언할 수 있다., Hooks로 바꿀 때에도 영향을 받지 않아 독립적으로 존재 가능
    const candidate = [1,2,3,4,5,6,7,8,9];
    const array = [];
    for (let i = 0; i < 4; i += 1){
@@ -11,43 +11,35 @@ function getNumbers(){ // 숫자 네개를 겹치지 않고 랜덤하게 뽑는 
    return array;
 }
 
-class NumberBaseball extends Component {
-   state = {
-      result: '',
-      value: '',
-      answer: getNumbers(),
-      tries: [], // push 쓰면 안된다. 불변성과 관련, react가 무엇이 바뀌었는지 감지를 하지 못한다. 그래서 새로운 변수로 배열을 만들어서 기존의 배열을 복사한 후 새로운 값을 넣는다.
-      // React의 렌더링하는 기준이 예전 state랑 현재 state랑 다를 때 렌더링을 한다.
-   };
+const NumberBaseball = () => {
+   const [result, setResult] = React.useState('');
+   const [value, setValue] = React.useState('');
+   const [answer, setAnswer] = React.useState(getNumbers); //getNumbers()이렇게 넣으면 안된다. 함수컴포넌트 특성 상 전체가 리렌더링 되는데 getNumbers()는 처음 한번만 적용되기 때문에 쓸데없이 매번 실행된다.
+   // useState는 값을 넣으면 data에 값이 설정되지만 함수를 넣으면 함수의 return 값이 data로 들어가고 그 다음부터는 실행되지 않는다.
+   //getNumbers() 이렇게 써도 문제되지는 않는다 초기값이기 때문에 useState가 알아서 첫번째 값만 넣어준다. 다만, 쓸데없이 리렌더링 되는게 문제다. 비효율적임 lazy init이라고 한다.
+   const [tries, setTries] = React.useState([]);
 
-   onSubmit = (e) => { // 화살표 함수를 사용하지 않으면 this를 사용할 때 에러가 생긴다. 화살표함수를 쓰지 않으려면 constructor를 사용해야한다.
-      const { result, value, tries, answer } = this.state; // 구조분해로 this.state를 지울 수 있다.
+   const onSubmit = (e) => { // 화살표 함수를 사용하지 않으면 this를 사용할 때 에러가 생긴다. 화살표함수를 쓰지 않으려면 constructor를 사용해야한다.
       e.preventDefault();
       if(value === answer.join('')){ // 답을 비교
-         this.setState({
-            result: '홈런!',
-            tries: [...tries, { try: value, result: '홈런!' }]
+         setTries((prevTries) => { // 옛날 값으로 현재 값을 할 때에는 함수형 setState 사용
+            return [...prevTries, { try: value, result: '홈런!' }]
          });
+         setResult('홈런!');
          alert('게임을 다시 시작합니다!');
-         this.setState({ // 초기화 하여 숫자를 새로 뽑음
-            value: '',
-            answer: getNumbers(),
-            tries: [],
-         });
+         setValue('');
+         setAnswer(getNumbers());
+         setTries([]);
       } else { // 답이 틀렸을 때
          const answerArray = value.split('').map((v) => parseInt(v));
          let strike = 0;
          let ball = 0;
          if(tries.length >= 9){ // 10번 이상 틀렸을 때
-            this.setState({
-               result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다.`,
-            });
+            setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')}였습니다.`,);
             alert('게임을 다시 시작합니다!');
-            this.setState({ // 초기화 하여 숫자를 새로 뽑음
-               value: '',
-               answer: getNumbers(),
-               tries: [],
-            });
+            setValue('');
+            setAnswer(getNumbers());
+            setTries([]);
          } else { // 10번 이내로 틀렸을 때
             for (let i = 0; i < 4; i += 1){
                if (answerArray[i] === answer[i]){
@@ -56,41 +48,37 @@ class NumberBaseball extends Component {
                   ball += 1;
                }
             }
-            this.setState({ // 몇 볼 몇 스트라이크인지 알려주고 기회 더 준다.
-               tries: [...tries, {try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}],
-               value: '',
+            setTries((prevTries) => { // 몇 볼 몇 스트라이크인지 알려주고 기회 더 준다.
+               return [...prevTries, {try: value, result: `${strike} 스트라이크, ${ball} 볼입니다.`}]
             });
+            setValue('');
          }
       }
    }
 
-   onChange = (e) => {
-      console.log(this.state.answer);
-      this.setState({
-         value: e.target.value,
-      });
+   const onChange = (e) => {
+      console.log(answer);
+      setValue(e.target.value);
    }
 
-   render(){
-      const { result, value, tries} = this.state; // 구조분해로 this.state를 지울 수 있다.
-      return(
-         <>
-            <h1>{result}</h1>
-            <form onSubmit={this.onSubmit}>
-               <input maxLength={4} value={value} onChange={this.onChange} />
-               <button type='submit'>입력</button>
-            </form>
-            <div>시도: {tries.length}</div>
-            <ul>
-               {tries.map((v, i) => { // v = {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.`} 이런 객체가 된다.
-                  return (
-                     <Try key={`${i + 1}차 시도:`} tryInfo={v} />
-                  );
-               })}
-            </ul>
-         </>
-      );
-   }
+
+   return(
+      <>
+         <h1>{result}</h1>
+         <form onSubmit={onSubmit}>
+            <input maxLength={4} value={value} onChange={onChange} />
+            <button>입력</button>
+         </form>
+         <div>시도: {tries.length}</div>
+         <ul>
+            {tries.map((v, i) => { // v = {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.`} 이런 객체가 된다.
+               return (
+                  <Try key={`${i + 1}차 시도:`} tryInfo={v} />
+               );
+            })}
+         </ul>
+      </>
+   );
 }
 
 //es2015문법, 노드의 모듈과 es2015의 모듈은 다르지만 일부분 호환이 되어서 이렇게 사용가능함.
--- React-02/숫자야구/Try.jsx
+++ React-02/숫자야구/Try.jsx
@@ -1,15 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Try extends Component { // Component를 사용하는 이유는 재사용성 때문이다.
-   render() {
-      const { tryInfo } = this.props; // 비구조화 할당으로 Hooks처럼 만들 수 있다.
+const Try = ({ tryInfo }) => { // Component를 사용하는 이유는 재사용성 때문이다. , props를 {tryInfo}로 구조분해 할 수 있다.
       return (
          <li>
             <div>{tryInfo.try}</div>
             <div>{tryInfo.result}</div>
          </li>
       );
-   }
 }
 
 export default Try;
