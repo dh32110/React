4d627dbb55417b03d9e6981f6e91d6b209b220db
dh32110 <dh32110@naver.com>
2023-06-16 21:23:55 +0900

    custom Hooks

--- React-02/rsp/useInterval.js
+++ React-02/rsp/useInterval.js
@@ -1,14 +1,14 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 // 커스텀 훅 - 어떤 특정한 훅 두개 이상이 반복될 때 보통 커스텀 훅을 만든다.
 
 //  활용의 예시
 // const [isRunning, setRunning] = useState(true);
-// useInterval(() => {
+// useInterval(() => { //callback
 //    console.log('hello');
 // }, isRunning ? 1000 : null); setRunning이 false를 하는 순간 isRunning false가 되고 그럼 null이 된다.
 
 function useInterval(callback, delay) {
-   const savedCallback = useRef();
+   const savedCallback = useRef(); //callback을 여기에 저장
 
    useEffect(() => {
       savedCallback.current = callback;
@@ -16,11 +16,11 @@ function useInterval(callback, delay) {
 
    useEffect(() => {
       function tick() {
-         savedCallback.current();
+         savedCallback.current(); // callback을 실행 
       }
 
-      if (delay !== null){
-         let id = setInterval(tick, delay);
+      if (delay !== null){ // delay가 null이 아닐 경우
+         let id = setInterval(tick, delay); // 실행
          return () => clearInterval(id);
       }
    }, [delay]);
